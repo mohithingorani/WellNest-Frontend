d29204a7bf2dade7888a07065304386717a52177
Mohit Hingorani <Mohithingorani2003@gmail.com>
2025-03-25 22:24:27 +0530

    made small change

--- src/components/OfferingCard.tsx
+++ src/components/OfferingCard.tsx
@@ -27,7 +27,7 @@ export default function OfferingCard({
       style={{
         zIndex: z_axisFinal,
       }}
-      className={`w-[200px] transition-transform duration-200 ease-out hover:scale-105 relative  h-[250px] rounded-2xl shadow-xl shadow-[#299b95] bg-white border-0.5 border-black ${className}`}
+      className={`w-[200px] cursor-pointer transition-transform duration-200 ease-out hover:scale-105 relative  h-[250px] rounded-2xl shadow-xl shadow-[#299b95] bg-white border-0.5 border-black ${className}`}
     >
       <div className="flex font-bold flex-col justify-end items-center h-full pb-4 text-[#09615d]">
         <div>
